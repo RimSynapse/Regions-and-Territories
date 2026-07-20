16a540a1ff22d337670f73c0c3402413fb8f5a38
archdukejim <sealteamchurch@gmail.com>
2026-07-19 23:09:30 -0400

    feat: implement DwellingStructureGenerator for procedural village dwelling and barracks spawning in RimWorld

--- Source/DwellingStructureGenerator.cs
+++ Source/DwellingStructureGenerator.cs
@@ -53,7 +53,7 @@ namespace RimSynapse.RegionsAndTerritories
             // 1. Generate Individual Dwellings (5x5 exterior, 3x3 interior)
             for (int i = 0; i < targetDwellings; i++)
             {
-                IntVec3 startSpot = FindValidSpot(map, spawnedBaseLocs);
+                IntVec3 startSpot = FindValidSpot(map, spawnedBaseLocs, 25);
                 if (startSpot == IntVec3.Invalid) continue;
 
                 IntVec3 baseLoc = startSpot - new IntVec3(8, 0, 8);
@@ -122,80 +122,55 @@ namespace RimSynapse.RegionsAndTerritories
                 SpawnThing(campfireDef, null, campfireLoc, map, faction);
 
                 System.Random random = new System.Random(baseLoc.x ^ baseLoc.z);
-                bool spawnPen = random.NextDouble() < 0.5;
+                
+                // Crop field (people always have a growing field, no pasture here)
+                Zone_Growing zone = new Zone_Growing(map.zoneManager);
+                map.zoneManager.RegisterZone(zone);
 
-                if (spawnPen)
+                ThingDef selectedCrop;
+                if (Rand.Value < 0.80f)
                 {
-                    // An empty 8x8 penned area
-                    for (int x = baseLoc.x; x <= baseLoc.x + 7; x++)
-                    {
-                        for (int z = baseLoc.z + 9; z <= baseLoc.z + 16; z++)
-                        {
-                            IntVec3 c = new IntVec3(x, 0, z);
-                            if (!c.InBounds(map)) continue;
-
-                            ClearBlockers(c, map);
-
-                            bool isBorderX = (x == baseLoc.x || x == baseLoc.x + 7);
-                            bool isBorderZ = (z == baseLoc.z + 9 || z == baseLoc.z + 16);
-
-                            if (isBorderX || isBorderZ)
-                            {
-                                if (x == baseLoc.x + 4 && z == baseLoc.z + 9)
-                                {
-                                    SpawnThing(fenceGateDef, woodLog, c, map, faction);
-                                }
-                                else
-                                {
-                                    SpawnThing(fenceDef, woodLog, c, map, faction);
-                                }
-                            }
-                        }
-                    }
-
-                    if (penMarkerDef != null)
-                    {
-                        IntVec3 markerLoc = new IntVec3(baseLoc.x + 3, 0, baseLoc.z + 12);
-                        ClearBlockers(markerLoc, map);
-                        SpawnThing(penMarkerDef, woodLog, markerLoc, map, faction);
-                    }
+                    // 80% chance: Edible crop (Potato, Corn, Rice)
+                    var edibleCrops = new List<ThingDef> { ThingDefOf.Plant_Potato };
+                    var corn = DefDatabase<ThingDef>.GetNamed("Plant_Corn", false);
+                    if (corn != null) edibleCrops.Add(corn);
+                    var rice = DefDatabase<ThingDef>.GetNamed("Plant_Rice", false);
+                    if (rice != null) edibleCrops.Add(rice);
+                    
+                    selectedCrop = edibleCrops[random.Next(edibleCrops.Count)];
                 }
                 else
                 {
-                    // Crop field
-                    Zone_Growing zone = new Zone_Growing(map.zoneManager);
-                    map.zoneManager.RegisterZone(zone);
-
-                    ThingDef[] cropsList = new ThingDef[]
-                    {
-                        ThingDefOf.Plant_Potato,
-                        DefDatabase<ThingDef>.GetNamed("Plant_Corn", false),
-                        DefDatabase<ThingDef>.GetNamed("Plant_Rice", false),
-                        DefDatabase<ThingDef>.GetNamed("Plant_Cotton", false),
-                        DefDatabase<ThingDef>.GetNamed("Plant_Healroot", false)
-                    };
+                    // 20% chance: Non-edible crop (Cotton, Healroot)
+                    var nonEdibleCrops = new List<ThingDef>();
+                    var cotton = DefDatabase<ThingDef>.GetNamed("Plant_Cotton", false);
+                    if (cotton != null) nonEdibleCrops.Add(cotton);
+                    var healroot = DefDatabase<ThingDef>.GetNamed("Plant_Healroot", false);
+                    if (healroot != null) nonEdibleCrops.Add(healroot);
                     
-                    List<ThingDef> validCrops = cropsList.Where(crop => crop != null).ToList();
-                    ThingDef selectedCrop = validCrops.Count > 0 ? validCrops[random.Next(validCrops.Count)] : ThingDefOf.Plant_Potato;
-
-                    for (int x = baseLoc.x + 6; x <= baseLoc.x + 13; x++)
-                    {
-                        for (int z = baseLoc.z; z <= baseLoc.z + 7; z++)
-                        {
-                            IntVec3 c = new IntVec3(x, 0, z);
-                            if (!c.InBounds(map)) continue;
-
-                            ClearBlockers(c, map);
-                            map.terrainGrid.SetTerrain(c, soilTerrain);
-                            zone.AddCell(c);
-
-                            Plant plant = (Plant)ThingMaker.MakeThing(selectedCrop);
-                            plant.Growth = Rand.Range(0.15f, 0.85f);
-                            GenSpawn.Spawn(plant, c, map);
-                        }
-                    }
-                    zone.SetPlantDefToGrow(selectedCrop);
+                    if (nonEdibleCrops.Count > 0)
+                        selectedCrop = nonEdibleCrops[random.Next(nonEdibleCrops.Count)];
+                    else
+                        selectedCrop = ThingDefOf.Plant_Potato;
                 }
+
+                for (int x = baseLoc.x + 6; x <= baseLoc.x + 13; x++)
+                {
+                    for (int z = baseLoc.z; z <= baseLoc.z + 7; z++)
+                    {
+                        IntVec3 c = new IntVec3(x, 0, z);
+                        if (!c.InBounds(map)) continue;
+
+                        ClearBlockers(c, map);
+                        map.terrainGrid.SetTerrain(c, soilTerrain);
+                        zone.AddCell(c);
+
+                        Plant plant = (Plant)ThingMaker.MakeThing(selectedCrop);
+                        plant.Growth = Rand.Range(0.15f, 0.85f);
+                        GenSpawn.Spawn(plant, c, map);
+                    }
+                }
+                zone.SetPlantDefToGrow(selectedCrop);
 
                 // Spawn resident pawn
                 if (Rand.Value < 0.66f)
@@ -204,32 +179,36 @@ namespace RimSynapse.RegionsAndTerritories
                 }
             }
 
-            // 2. Generate Barracks (5x9 exterior, 3x7 interior)
+            // 2. Generate Barracks (5x9 exterior, 3x7 interior) - Spacing is 50x50
             for (int i = 0; i < targetBarracks; i++)
             {
-                IntVec3 startSpot = FindValidSpot(map, spawnedBaseLocs);
+                IntVec3 startSpot = FindValidSpot(map, spawnedBaseLocs, 50);
                 if (startSpot == IntVec3.Invalid) continue;
 
-                IntVec3 baseLoc = startSpot - new IntVec3(8, 0, 8);
+                // Spacing base is 50x50; offset barracks inside it to make room for the 9x9 pasture
+                IntVec3 baseLoc = startSpot - new IntVec3(16, 0, 16);
                 spawnedBaseLocs.Add(baseLoc);
 
+                // Barracks building is placed at offset (5, 5) inside the 50x50 clearance zone
+                IntVec3 buildBase = baseLoc + new IntVec3(5, 0, 5);
+
                 // Footprint is 5x9 (width 9 on X, height 5 on Z)
-                for (int x = baseLoc.x; x <= baseLoc.x + 8; x++)
+                for (int x = buildBase.x; x <= buildBase.x + 8; x++)
                 {
-                    for (int z = baseLoc.z; z <= baseLoc.z + 4; z++)
+                    for (int z = buildBase.z; z <= buildBase.z + 4; z++)
                     {
                         IntVec3 c = new IntVec3(x, 0, z);
                         if (!c.InBounds(map)) continue;
 
                         ClearBlockers(c, map);
 
-                        bool isBorderX = (x == baseLoc.x || x == baseLoc.x + 8);
-                        bool isBorderZ = (z == baseLoc.z || z == baseLoc.z + 4);
+                        bool isBorderX = (x == buildBase.x || x == buildBase.x + 8);
+                        bool isBorderZ = (z == buildBase.z || z == buildBase.z + 4);
 
                         if (isBorderX || isBorderZ)
                         {
-                            // Door in the center of the 9-length wall (at bottom, z = baseLoc.z)
-                            if (x == baseLoc.x + 4 && z == baseLoc.z)
+                            // Door in the center of the 9-length wall (at bottom, z = buildBase.z)
+                            if (x == buildBase.x + 4 && z == buildBase.z)
                             {
                                 SpawnThing(doorDef, woodLog, c, map, faction);
                             }
@@ -248,12 +227,12 @@ namespace RimSynapse.RegionsAndTerritories
                 }
 
                 // Spawn 4 beds lengthwise towards the door (North rotation, headboard against far top wall)
-                // Bed X spots: baseLoc.x + 1, baseLoc.x + 3, baseLoc.x + 5, baseLoc.x + 7
+                // Bed X spots: buildBase.x + 1, buildBase.x + 3, buildBase.x + 5, buildBase.x + 7
                 List<Building_Bed> barracksBeds = new List<Building_Bed>();
                 for (int bedIndex = 0; bedIndex < 4; bedIndex++)
                 {
-                    int bx = baseLoc.x + 1 + (bedIndex * 2);
-                    IntVec3 bLoc = new IntVec3(bx, 0, baseLoc.z + 2);
+                    int bx = buildBase.x + 1 + (bedIndex * 2);
+                    IntVec3 bLoc = new IntVec3(bx, 0, buildBase.z + 2);
                     ClearBlockers(bLoc, map);
                     ClearBlockers(bLoc + new IntVec3(0, 0, 1), map);
 
@@ -272,16 +251,90 @@ namespace RimSynapse.RegionsAndTerritories
                 }
 
                 // Campfire outside (near the door)
-                IntVec3 campfireLoc = new IntVec3(baseLoc.x + 4, 0, baseLoc.z - 2);
+                IntVec3 campfireLoc = new IntVec3(buildBase.x + 4, 0, buildBase.z - 2);
                 ClearBlockers(campfireLoc, map);
                 SpawnThing(campfireDef, null, campfireLoc, map, faction);
+
+                // Generate a 9x9 pasture pen at offset (20, 20) inside the 50x50 clearance zone
+                IntVec3 pBase = baseLoc + new IntVec3(20, 0, 20);
+                for (int x = pBase.x; x <= pBase.x + 8; x++)
+                {
+                    for (int z = pBase.z; z <= pBase.z + 8; z++)
+                    {
+                        IntVec3 c = new IntVec3(x, 0, z);
+                        if (!c.InBounds(map)) continue;
+
+                        ClearBlockers(c, map);
+
+                        bool isBorderX = (x == pBase.x || x == pBase.x + 8);
+                        bool isBorderZ = (z == pBase.z || z == pBase.z + 8);
+
+                        if (isBorderX || isBorderZ)
+                        {
+                            if (x == pBase.x + 4 && z == pBase.z)
+                            {
+                                SpawnThing(fenceGateDef, woodLog, c, map, faction);
+                            }
+                            else
+                            {
+                                SpawnThing(fenceDef, woodLog, c, map, faction);
+                            }
+                        }
+                        else
+                        {
+                            // Spawn biome-specific grazing plants
+                            if (Rand.Value < 0.7f)
+                            {
+                                ThingDef plantDef = FindGrazingPlant(map);
+                                Plant plant = (Plant)ThingMaker.MakeThing(plantDef);
+                                plant.Growth = Rand.Range(0.4f, 0.9f);
+                                GenSpawn.Spawn(plant, c, map);
+                            }
+                        }
+                    }
+                }
+
+                if (penMarkerDef != null)
+                {
+                    IntVec3 markerLoc = pBase + new IntVec3(4, 0, 4);
+                    ClearBlockers(markerLoc, map);
+                    SpawnThing(penMarkerDef, woodLog, markerLoc, map, faction);
+                }
+
+                // Spawn a male and a female grazing animal of a biome-suitable kind inside the pasture
+                PawnKindDef animalKind = FindGrazingAnimalKind(map);
+                if (animalKind != null)
+                {
+                    IntVec3 animalLoc1 = pBase + new IntVec3(2, 0, 2);
+                    IntVec3 animalLoc2 = pBase + new IntVec3(6, 0, 6);
+                    ClearBlockers(animalLoc1, map);
+                    ClearBlockers(animalLoc2, map);
+
+                    PawnGenerationRequest maleRequest = new PawnGenerationRequest(
+                        animalKind,
+                        faction: faction,
+                        fixedGender: Gender.Male,
+                        context: PawnGenerationContext.NonPlayer
+                    );
+                    Pawn maleAnimal = PawnGenerator.GeneratePawn(maleRequest);
+                    GenSpawn.Spawn(maleAnimal, animalLoc1, map);
+
+                    PawnGenerationRequest femaleRequest = new PawnGenerationRequest(
+                        animalKind,
+                        faction: faction,
+                        fixedGender: Gender.Female,
+                        context: PawnGenerationContext.NonPlayer
+                    );
+                    Pawn femaleAnimal = PawnGenerator.GeneratePawn(femaleRequest);
+                    GenSpawn.Spawn(femaleAnimal, animalLoc2, map);
+                }
 
                 // Spawn residents for barracks (each gets assigned to one of the 4 beds)
                 for (int bIndex = 0; bIndex < barracksBeds.Count; bIndex++)
                 {
                     if (Rand.Value < 0.75f)
                     {
-                        IntVec3 pLoc = new IntVec3(baseLoc.x + 1 + (bIndex * 2), 0, baseLoc.z + 1);
+                        IntVec3 pLoc = new IntVec3(buildBase.x + 1 + (bIndex * 2), 0, buildBase.z + 1);
                         SpawnResident(map, pLoc, barracksBeds[bIndex]);
                     }
                 }
@@ -405,22 +458,80 @@ namespace RimSynapse.RegionsAndTerritories
             );
         }
 
-        private static IntVec3 FindValidSpot(Map map, List<IntVec3> spawnedBaseLocs)
+        private static PawnKindDef FindGrazingAnimalKind(Map map)
         {
+            try
+            {
+                if (map?.Biome?.AllWildAnimals != null)
+                {
+                    var eligible = map.Biome.AllWildAnimals
+                        .Where(k => k.RaceProps != null && 
+                                    !k.RaceProps.ToolUser && 
+                                    k.RaceProps.Eats(FoodTypeFlags.Plant) && 
+                                    !k.RaceProps.predator)
+                        .ToList();
+                    
+                    if (eligible.Count > 0)
+                    {
+                        return eligible[Rand.Range(0, eligible.Count)];
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // Fallback
+            }
+
+            return DefDatabase<PawnKindDef>.GetNamed("Muffalo", false) 
+                ?? DefDatabase<PawnKindDef>.GetNamed("Sheep", false) 
+                ?? DefDatabase<PawnKindDef>.AllDefs.FirstOrDefault(k => k.RaceProps != null && k.RaceProps.Eats(FoodTypeFlags.Plant) && !k.RaceProps.predator);
+        }
+
+        private static ThingDef FindGrazingPlant(Map map)
+        {
+            try
+            {
+                if (map?.Biome?.wildPlants != null)
+                {
+                    var eligible = map.Biome.wildPlants
+                        .Where(p => p.plant != null && 
+                                    p.plant.plant != null && 
+                                    !p.plant.plant.IsTree && 
+                                    (p.plant.defName.Contains("Grass") || p.plant.defName.Contains("Dandelion")))
+                        .Select(p => p.plant)
+                        .ToList();
+                    
+                    if (eligible.Count > 0)
+                    {
+                        return eligible[Rand.Range(0, eligible.Count)];
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // Fallback
+            }
+
+            return ThingDefOf.Plant_Grass;
+        }
+
+        private static IntVec3 FindValidSpot(Map map, List<IntVec3> spawnedBaseLocs, int size)
+        {
+            int offset = size / 3;
             for (int i = 0; i < 3000; i++)
             {
                 IntVec3 cell = CellFinder.RandomCell(map);
                 if (cell.Walkable(map) && 
                     !cell.Fogged(map) && 
                     cell.GetTerrain(map).affordances.Contains(TerrainAffordanceDefOf.Heavy) && 
-                    cell.x > 25 && cell.x < map.Size.x - 25 && cell.z > 25 && cell.z < map.Size.z - 25)
+                    cell.x > size && cell.x < map.Size.x - size && cell.z > size && cell.z < map.Size.z - size)
                 {
-                    IntVec3 baseLoc = cell - new IntVec3(8, 0, 8);
+                    IntVec3 baseLoc = cell - new IntVec3(offset, 0, offset);
                     
                     bool tooClose = false;
                     foreach (var other in spawnedBaseLocs)
                     {
-                        if (baseLoc.DistanceToSquared(other) < 25 * 25)
+                        if (baseLoc.DistanceToSquared(other) < size * size)
                         {
                             tooClose = true;
                             break;
@@ -429,9 +540,9 @@ namespace RimSynapse.RegionsAndTerritories
                     if (tooClose) continue;
 
                     bool clear = true;
-                    for (int x = -2; x <= 22; x++)
+                    for (int x = -2; x <= size - 3; x++)
                     {
-                        for (int z = -2; z <= 22; z++)
+                        for (int z = -2; z <= size - 3; z++)
                         {
                             IntVec3 c = baseLoc + new IntVec3(x, 0, z);
                             if (!c.InBounds(map))
